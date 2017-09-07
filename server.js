c1f6e46037312b3b133090bf527bf0b9ca1f92cc
Paul Hibbert <paul@hibbert.online>
2017-09-07 12:29:06 +0100

    initial commit

---
+++ server.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+app.use((req, res, next) => {
+    var now = new Date().toString();
+    var log = `${now}: ${req.method} ${req.url}`;
+    console.log(log);
+    fs.appendFile('server.log', log+'\n', (err) => {
+        if(err){
+            console.log('Unable to append to server.log');
+        }
+    });
+    next();
+});
+
+// app.use((req, res, next) => {
+//     res.render('maintenance.hbs'); //stop here do not call next()
+// });
+
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear()
+});
+
+hbs.registerHelper('screamIt', (text) => {
+    return text.toUpperCase()
+});
+
+app.get('/', (request, response) => {
+    response.render('home.hbs', {
+        title: 'Home Page',
+        link: '/about',
+        linkName: 'About',
+        welcome: 'Welcome to this test website using Node/Express'
+    });
+});
+
+app.get('/about', (request, response)=> {
+    // response.send('<h1>About Page</h1>');
+    response.render('about.hbs',{
+        title: 'About Page template',
+        link: '/',
+        linkName: 'Home',
+    });
+});
+
+app.get('/bad', (request, response) => {
+    // console.log(request);
+    response.send({
+        errorMessage: 'Unable to process request'
+    });
+});
+
+app.listen(3000, () => {
+    console.log('Server is up on port 3000')
+});
